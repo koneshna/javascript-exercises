29d6528847f182d2ab9ea72fd2a5d78cac3a88ff
koneshna <guditisn@gmail.com>
2022-11-14 19:32:10 -0500

    reversed array remade string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+  var splitString = string.split('');
+
+  var reverseArray = splitString.reverse();
+
+  var joinArray = reverseArray.join('')
+
+  return joinArray;
 };
 
+console.log(reverseString)
 // Do not edit below this line
 module.exports = reverseString;
